2ad2b89d5f678c4c306d3ffeb09fdfbb0227a960
xin00killo <xin00killo@163.com>
2020-01-18 18:06:57 +0800

    初始化

---
+++ common/wyx_requests.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+# __autor__='wyxces'
+
+import requests
+
+class WyxRequest():
+    def __init__(self):
+        self.url =
+    def wyx_request(self,):
+    def  wyx_get(self,url,param):
+        r = requests.get('https://www.baidu.com/')
+        print(r.content)
+        print(r.text)
