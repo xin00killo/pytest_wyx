5dfbb8aa6963a48b92b3faffdaf1417ebe41d967
xin00killo <xin00killo@163.com>
2020-01-31 22:14:17 +0800

    添加logs文件夹

---
+++ common/common/mysql_wyx.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Author  : wyx
+
+from common.common.singleton_wyx import singleton
+
+
+# 创建数据库链接池
+@singleton
+class MysqlWyx:
+    def __init__(self):
+        pass
+
+    def select(self):
+        pass
+
+    def delete(self):
+        pass
